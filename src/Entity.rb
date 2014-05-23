959240a30013a5b667f4d1ecc7371a0107306d7b
Freddan962 <fredde.sander@gmail.se>
2014-05-23 21:23:51 +0200

    Entity attr_accessors

--- src/Entity.rb
+++ src/Entity.rb
@@ -1,7 +1,11 @@
 class Entity
+  attr_accessor :velocity_x, :velocity_y
+
   def initialize(image, x, y, hp)
     @image = image
     @x, @y = x, y
     @hp = hp
+    @velocity_x, velocity_y = 0, 0
+    @alive = true
   end
 end
