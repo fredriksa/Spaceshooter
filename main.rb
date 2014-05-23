0724c850d8adc29451e7aca9eaff0e4d8f952151
Freddan962 <fredde.sander@gmail.se>
2014-05-23 20:29:41 +0200

    Declared and initialized GameWindow::NAME

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
 require 'gosu'
 
 class GameWindow < Gosu::Window
-  HEIGHT, WIDTH, FULLSCREEN = 640, 480, false
+  HEIGHT, WIDTH, FULLSCREEN, NAME = 640, 480, false, "Spaceshooter"
   
   def initialize
     super HEIGHT, WIDTH, FULLSCREEN
   end
-
+  
 end
