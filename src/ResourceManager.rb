852351cd91978c651f0f5fc17a3d3c763bd2bc6f
Freddan962 <fredde.sander@gmail.se>
2014-05-23 21:05:03 +0200

    Defined ResourceManager::get(key)

--- src/ResourceManager.rb
+++ src/ResourceManager.rb
@@ -8,4 +8,8 @@ class ResourceManager
     raise IOError, 'path must point to a existing file!' if !File.file?(path)
     @resources[key] = Gosu::Image.new(@window, path, false) if @resources[key].nil?
   end
+
+  def get(key)
+    
+  end
 end
