5a82bdbe86cc19451e35af9d9ee94bd339ba4204
Freddan962 <fredde.sander@gmail.se>
2014-05-23 21:04:24 +0200

    ResourceManager::load_image IOError exception

--- src/ResourceManager.rb
+++ src/ResourceManager.rb
@@ -5,6 +5,7 @@ class ResourceManager
   end
 
   def load_image(key, path)
+    raise IOError, 'path must point to a existing file!' if !File.file?(path)
     @resources[key] = Gosu::Image.new(@window, path, false) if @resources[key].nil?
   end
 end
