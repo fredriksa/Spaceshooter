17bbb03e91af86ac7b4808e5d5967407de0a482e
Freddan962 <fredde.sander@gmail.se>
2014-05-24 12:21:04 +0200

    Declared and initialized player kbleft, kbrigt

--- src/Player.rb
+++ src/Player.rb
@@ -2,6 +2,13 @@ class Player < FloatingEntity
   WIDTH, HEIGHT = 27, 22
   attr_accessor :KbLeft, :KbRight
 
+  def initialize(images, x, y, hp, speed)
+    super images, x, y, hp, speed
+    @KbLeft = false
+    @KbRight = false
+  end
+
   def update
+    super
   end
 end
