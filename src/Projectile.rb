85cad2688cfe313ad16fc8c0ff79444cd1de2fb8
Freddan962 <fredde.sander@gmail.se>
2014-05-24 18:57:59 +0200

    type acessor

--- src/Projectile.rb
+++ src/Projectile.rb
@@ -1,7 +1,9 @@
 class Projectile < FloatingEntity
-  def initialize(image, x, y, hp, speed, velocity_y, speed_damper = 1)
+  attr_accessor :type
+  def initialize(image, x, y, hp, speed, velocity_y, type, speed_damper = 1)
     super image, x, y, hp, speed, speed_damper
     @velocity_y = velocity_y
+    @type = type
   end
 
   def update
