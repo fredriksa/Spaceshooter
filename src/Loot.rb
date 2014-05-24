1f929fb95626a870800652f1fc37e66bf656fd4d
Freddan962 <fredde.sander@gmail.se>
2014-05-24 19:22:23 +0200

    Declared and initialized image variable

--- src/Loot.rb
+++ src/Loot.rb
@@ -1,5 +1,6 @@
 class Loot < FloatingEntity
   def initialize(type, rm, x, y, hp, velocity_y, speed)
+    image = rm.get("loot.png")
     super image, x, y, hp, speed
     @velocity_y = velocity_y
   end
