a7cb6e1fdcaeb4a79d08ef3ac776ded0d6c315a6
Freddan962 <fredde.sander@gmail.se>
2014-05-24 11:32:06 +0200

    Added constructor parameter to Entity class

--- src/Entity.rb
+++ src/Entity.rb
@@ -1,10 +1,11 @@
 class Entity
   attr_accessor :velocity_x, :velocity_y
 
-  def initialize(image, x, y, hp)
+  def initialize(image, x, y, hp, speed)
     @image = image
     @x, @y = x, y
     @hp = hp
+    @speed = speed
     @velocity_x, @velocity_y = 0, 0
     @alive = true
   end
