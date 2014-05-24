28b2744f8ea7291f37af5cb99467c0c86fa6066a
Freddan962 <fredde.sander@gmail.se>
2014-05-24 19:29:43 +0200

    Declared and initialized instance variable type

--- src/Loot.rb
+++ src/Loot.rb
@@ -1,6 +1,8 @@
 class Loot < FloatingEntity
+  attr_accessor :type
   def initialize(type, image, x, y, hp, velocity_y, speed)
     super image, x, y, hp, speed
+    @type = type
     @velocity_y = velocity_y
   end
 end
