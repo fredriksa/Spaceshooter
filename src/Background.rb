8a97e53ad5e56475be9fa412c99ae47b87029614
Freddan962 <fredde.sander@gmail.se>
2014-05-24 13:54:47 +0200

    Background is now 'scrolling' forever.

--- src/Background.rb
+++ src/Background.rb
@@ -4,7 +4,9 @@ class Background < FloatingEntity
     @cover = cover
     @double_cover = double_cover
     
-    @y = 50
+    @y = 0
+    @y2 = -500
+
     @velocity_x, @velocity_y = 1, 1
     @speed, @speed_damper = 1, 1
   end
@@ -13,9 +15,19 @@ class Background < FloatingEntity
     @solid.draw(0, 0, ZOrder::Background_Solid)
     @cover.draw(0, @y, ZOrder::Background_Cover)
     @cover.draw(200, @y, ZOrder::Background_Cover)if @double_cover
+    @cover.draw(0, @y2, ZOrder::Background_Cover)
+    @cover.draw(200, @y2, ZOrder::Background_Cover)
   end
 
   def update
     move_y
+
+    @y = -500 if @y > GameWindow::HEIGHT
+    @y2 = -500 if @y2 > GameWindow::HEIGHT
+  end
+
+  def move_y
+    super
+    @y2 += @velocity_y
   end
 end
