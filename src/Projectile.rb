b2fb5f84394691c6aebe14f47b2a460161127e31
Freddan962 <fredde.sander@gmail.se>
2014-05-24 15:17:03 +0200

    Renamed argument images to image

--- src/Projectile.rb
+++ src/Projectile.rb
@@ -1,6 +1,6 @@
 class Projectile < FloatingEntity
-  def initialize(images, x, y, hp, speed, velocity_y, speed_damper = 1)
-    super images, x, y, hp, speed, speed_damper
+  def initialize(image, x, y, hp, speed, velocity_y, speed_damper = 1)
+    super image, x, y, hp, speed, speed_damper
     @velocity_y = velocity_y
   end
 
