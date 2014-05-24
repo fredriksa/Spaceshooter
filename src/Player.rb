9bc5b080af02ad65ce30efcd0f398d50a307076b
Freddan962 <fredde.sander@gmail.se>
2014-05-24 21:50:07 +0200

    width and height instance variables

--- src/Player.rb
+++ src/Player.rb
@@ -1,11 +1,12 @@
 class Player < SpaceshipFighter
   WIDTH, HEIGHT = 27, 22
-  attr_accessor :KbLeft, :KbRight
+  attr_accessor :KbLeft, :KbRight, :width, :height
 
   def initialize(images, projectile_image, x, y, hp, speed, speed_damper = 1)
     super images, projectile_image, x, y, WIDTH, HEIGHT, hp, speed, "Player", speed_damper
     @KbLeft = false
     @KbRight = false
+    @width, @height = WIDTH, HEIGHT
   end
 
   def update
