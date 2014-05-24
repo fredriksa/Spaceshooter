b09538bb5673af22586d7a840b80f4f3fa2c20f9
Freddan962 <fredde.sander@gmail.se>
2014-05-24 22:03:50 +0200

    Now returns surrounding_objects

--- src/ContentManager.rb
+++ src/ContentManager.rb
@@ -13,9 +13,11 @@ class ContentManager
   end
 
   def get_surrounding_objects(object)
+    surrounding_objects = []
     ((object.x/@grid_width-1).round..(object.x/@grid_width+1).round).each do |x|
       ((object.y/@grid_height-1).round..(object.y/@grid_height+1).round).each do |y|
         if @content_grid[y][x] != nil and object != @content_grid[y][x]
+          surrounding_objects << @content_grid[y][x]
         end
       end
     end
