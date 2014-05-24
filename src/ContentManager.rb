4646a06d082746bb565102cee1f2cfe09d1d1150
Freddan962 <fredde.sander@gmail.se>
2014-05-24 18:12:15 +0200

    Revised ContentManager algorithm

--- src/ContentManager.rb
+++ src/ContentManager.rb
@@ -6,22 +6,23 @@ class ContentManager
   end
 
   def update(objects)
-    @content_grid = Array.new(GameWindow::HEIGHT/@grid_height + 1) { Array.new(GameWindow::WIDTH/@grid_width + 1) { nil }}
+    clean_array
     objects.each do |object|
       @content_grid[object.y/@grid_height][object.x/@grid_width] = object
     end  
   end
 
   def get_surrounding_objects(object)
-    for x in object.x/@grid_width-1.round..object.x/@grid_width+1.round
-      puts x
-      for y in object.y/@grid_height-1.round..object.y/@grid_height+1.round
-        puts y
-        if @content_grid[y][x] != nil
-          puts "Found object"
+    ((object.x/@grid_width-1).round..(object.x/@grid_width+1).round).each do |x|
+      ((object.y/@grid_height-1).round..(object.y/@grid_height+1).round).each do |y|
+        if @content_grid[y][x] != nil and object != @content_grid[y][x]
           p @content_grid[y][x]
         end
       end
     end
+  end
+
+  def clean_array
+    @content_grid = Array.new(GameWindow::HEIGHT/@grid_height + 1) { Array.new(GameWindow::WIDTH/@grid_width + 1) { nil }}
   end
 end
