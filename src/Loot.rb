b4df742445a81a43fe171e2acfa79c171279d776
Freddan962 <fredde.sander@gmail.se>
2014-05-24 19:51:07 +0200

    Added body of get_health, get_ammo

--- src/Loot.rb
+++ src/Loot.rb
@@ -4,11 +4,18 @@ class Loot < FloatingEntity
     super image, x, y, hp, speed
     @type = type
     @velocity_y = velocity_y
+
+    @health = 50 if @type == "Health"
+    @ammo = 25 if @type == "Ammo"
   end
 
   def get_health
+    health, @health = @health, 0
+    return health
   end
 
   def get_ammo
+    ammo, @ammo = @ammo, 0
+    return ammo
   end
 end
