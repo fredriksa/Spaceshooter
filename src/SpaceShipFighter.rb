5839ce6f072b10a85eab0ca07893448fae6894e1
Freddan962 <fredde.sander@gmail.se>
2014-05-24 14:47:39 +0200

    Declared and initialized instance variable projectiles

--- src/SpaceShipFighter.rb
+++ src/SpaceShipFighter.rb
@@ -1,6 +1,7 @@
 class SpaceshipFighter < FloatingEntity
   def initializedef(images, x, y, hp, speed, speed_damper = 1)
     super images, x, y, hp, speed, speed_damper
+    @projectiles = []
     @damage = 25
   end
 
