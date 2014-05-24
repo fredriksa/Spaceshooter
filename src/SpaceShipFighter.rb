f99464baadac735644ec65f2d2640bfc67247f36
Freddan962 <fredde.sander@gmail.se>
2014-05-24 14:47:02 +0200

    Declared and initialized instance variable damage

--- src/SpaceShipFighter.rb
+++ src/SpaceShipFighter.rb
@@ -1,7 +1,9 @@
 class SpaceshipFighter < FloatingEntity
-  def initialize
-
+  def initializedef(images, x, y, hp, speed, speed_damper = 1)
+    super images, x, y, hp, speed, speed_damper
+    @damage = 25
   end
+
   def fire
     
   end
