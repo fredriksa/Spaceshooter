39cda1ee8992669249ac3c75cae0cbc35fea1104
Freddan962 <fredde.sander@gmail.se>
2014-05-24 20:33:25 +0200

    Small change objects

--- src/gamestates/play.rb
+++ src/gamestates/play.rb
@@ -4,6 +4,8 @@ class Play < GameState
     @speed = 1
     @player = Player.new(@rm.get("player", "player_left", "player_right"), @rm.get("player_projectile"), GameWindow::WIDTH/2 - Player::WIDTH/2, GameWindow::HEIGHT - 50, 100, 1.5, 0.95)
     @background = Background.new(@rm.get("background_solid"), @rm.get("background_stars"), @speed, true)
+    @objects << @player
+    @objects << @background
     @loot_spawn_timer, @loot_spawn_counter = 5*60, 0
   end
 
@@ -13,9 +15,6 @@ class Play < GameState
 
   def update
     super
-    @objects = []
-    @objects << @player
-    @objects << @background
     @objects += @player.projectiles
     
     if @loot_spawn_counter > @loot_spawn_timer
@@ -24,7 +23,7 @@ class Play < GameState
     else
       @loot_spawn_counter += 1
     end
-
+    
     @cm.update(@objects)
     @cm.get_surrounding_objects(@player)
   end
