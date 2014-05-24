69414010fac33c091df6060768e7bb1fe93f8423
Freddan962 <fredde.sander@gmail.se>
2014-05-24 15:19:51 +0200

    require Projectile.rb

--- main.rb
+++ main.rb
@@ -7,6 +7,7 @@ require_relative 'src/ResourceManager.rb'
 require_relative 'src/Entity.rb'
 require_relative 'src/FloatingEntity.rb'
 require_relative 'src/SpaceshipFighter.rb'
+require_relative 'src/Projectile.rb'
 
 require_relative 'src/Player.rb'
 require_relative 'src/Background.rb'
