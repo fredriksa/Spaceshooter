679f749569672b844a12c356fd993c70c8d9d138
Freddan962 <fredde.sander@gmail.se>
2014-05-24 19:16:43 +0200

    Class variables

--- src/Loot.rb
+++ src/Loot.rb
@@ -1,6 +1,8 @@
 class Loot < FloatingEntity
-  def initialize(type, image, x, y, hp, velocity_y, speed)
+  def initialize(type, x, y, hp, velocity_y, speed, images = 0)
     super image, x, y, hp, speed
+    @@types = {"Ammo" => 0, "Health" => 1}
+    @@images = images if @@images.nil?
     @velocity_y = 1
   end
 end
