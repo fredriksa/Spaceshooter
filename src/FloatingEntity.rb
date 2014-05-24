28ad58476740478ab59792ffbb5683d0997c3605
Freddan962 <fredde.sander@gmail.se>
2014-05-24 11:24:38 +0200

    Added vertical movement to FloatingEntity::move

--- src/FloatingEntity.rb
+++ src/FloatingEntity.rb
@@ -1,9 +1,15 @@
 class FloatingEntity
   def move
-    if @x + @velocity_x < GameWindow::WIDTH && @x + @velocity_x > 0
+    if @x + @velocity_x < GameWindow::WIDTH and @x + @velocity_x > 0
       @x += @velocity_x
       @x %= GameWindow::WIDTH
-      @velocity_x * 0.95
+      @velocity_x *= 0.95
+    end
+
+    if @y + @velocity_y < GameWindow::HEIGHT and @y + @velocity_y > 0
+      @y += @velocity_y
+      @y %= GameWindow::HEIGHT
+      @velocity_y *= 0.95
     end
   end
 end
