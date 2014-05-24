c907afebbc66571f64b55cab95f83ca55bd8cc3f
Freddan962 <fredde.sander@gmail.se>
2014-05-24 14:16:53 +0200

    removed @velocity_x

--- src/Projectile.rb
+++ src/Projectile.rb
@@ -1,7 +1,7 @@
 class Projectile < FloatingEntity
-  def initialize(images, x, y, hp, speed, velocity_x, velocity_y, speed_damper = 1)
+  def initialize(images, x, y, hp, speed, velocity_y, speed_damper = 1)
     super images, x, y, hp, speed, speed_damper
-    @veloicty_x, @velocity_y = velocity_x, velocity_y
+    @velocity_y = velocity_y
   end
 
   def update
