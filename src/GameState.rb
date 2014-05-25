037a4d727e4f6407d211ed237e3d8921217d37d6
Freddan962 <fredde.sander@gmail.se>
2014-05-25 19:40:58 +0200

    Raises IOError if its resource file does not exist

--- src/GameState.rb
+++ src/GameState.rb
@@ -19,7 +19,7 @@ class GameState
   end
 
   def on_load
-    raise IOerror, 'file does not exist' if !File.file?(self.class.to_s + "_resources.txt")
+    raise IOError, 'file does not exist' if !File.file?("./src/gamestates/" + self.class.to_s + "_resources.txt")
   end
 
   def on_exit
