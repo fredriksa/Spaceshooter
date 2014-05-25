d5c6068e037345c823a7d200508b06feee602e15
Freddan962 <fredde.sander@gmail.se>
2014-05-25 17:36:50 +0200

    Initializes instance variables

--- src/GUIItem.rb
+++ src/GUIItem.rb
@@ -1,10 +1,12 @@
 class GUIItem
-  def initialize(image, x, y)
-  
+  def initialize(image, x, y, width, height)
+    @image = image
+    @x, @y = x, y
+    @width, @height = image.height, image.width
   end
 
   def draw
-
+  
   end
 
   def update
