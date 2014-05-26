e126e9ea2bf0023f3ea57fff7763d8283553c28f
Freddan962 <fredde.sander@gmail.se>
2014-05-26 19:03:34 +0200

    Now draws flames

--- src/Player.rb
+++ src/Player.rb
@@ -3,8 +3,21 @@ class Player < SpaceshipFighter
 
   def initialize(images, projectile_image, x, y, hp, speed, speed_damper = 1)
     super images, projectile_image, x, y, hp, speed, "Player", speed_damper
+    @flame = images[3]
     @KbLeft = false
     @KbRight = false
+  end
+
+  def draw
+    super
+    if @velocity_x > 2
+      @flame.draw(@x+4.5, @y+17, ZOrder::Entity_Secondary)
+    elsif @velocity_x < -2
+      @flame.draw(@x+17.5, @y+17, ZOrder::Entity_Secondary)
+    else
+      @flame.draw(@x+4.5, @y+17, ZOrder::Entity_Secondary)
+      @flame.draw(@x+17.5, @y+17, ZOrder::Entity_Secondary) 
+    end
   end
 
   def update
