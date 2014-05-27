5061556e70fea69592ad64c2ca051512aa3bf4de
Freddan962 <fredde.sander@gmail.se>
2014-05-27 23:35:54 +0200

    Iterates to max image index

--- src/Explosion.rb
+++ src/Explosion.rb
@@ -1,9 +1,16 @@
 class Explosion < Entity
+  def initialize(images, x, y)
+    super images, x, y
+    @counter = 0
+  end
+
   def update
     @counter += 1
-    if @counter >= 6
+    if @counter >= 1 and @image_index < 10
       @image_index += 1
       @counter = 0
+    else
+      die
     end
   end
 end
