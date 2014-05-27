d1be39861b598e6498ed604093e9bbbc613fecd2
Freddan962 <fredde.sander@gmail.se>
2014-05-27 20:36:09 +0200

    Required new GUI files

--- main.rb
+++ main.rb
@@ -13,6 +13,10 @@ require_relative 'src/Projectile.rb'
 require_relative 'src/Loot.rb'
 
 require_relative 'src/GUI/GUI.rb'
+require_relative 'src/GUI/Item.rb'
+require_relative 'src/GUI/Text.rb'
+require_relative 'src/GUI/Icon.rb'
+require_relative 'src/GUI/Button.rb'
 
 require_relative 'src/Player.rb'
 require_relative 'src/Background.rb'
