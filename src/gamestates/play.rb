36a0ede6d07ce47c5e95f8f1e12fb0fdd981cb4b
Freddan962 <fredde.sander@gmail.se>
2014-05-27 20:57:56 +0200

    Declare and initialize an instance of GUI::GUI

--- src/gamestates/play.rb
+++ src/gamestates/play.rb
@@ -6,6 +6,7 @@ class Play < GameState
     @speed = 1
     @player = Player.new(@rm.get("player", "player_left", "player_right", "player_flame"), @rm.get("player_projectile"), GameWindow::WIDTH/2, GameWindow::HEIGHT - 50, 100, 1.5, 0.95)
     @background = Background.new(@rm.get("background_solid"), @rm.get("background_stars"), @speed, true)
+    @GUI = GUI::GUI.new
     @objects << @player
     @objects << @background
     @loot_spawn_timer, @loot_spawn_counter = 1*60, 0
