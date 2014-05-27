44ea3b7b72c607b402f66e37d9e022368149eba9
Freddan962 <fredde.sander@gmail.se>
2014-05-28 01:24:03 +0200

    AI Logic

--- src/AIfighter/AIFighter.rb
+++ src/AIfighter/AIFighter.rb
@@ -1,7 +1,16 @@
 module AIFighter
   class AIFighter < SpaceshipFighter
     def update(objects)
-
+      objects = objects.dup
+      objects.drop_while {|object| object.class != Projectile && object.team != "Player"}
+      closest_projectile = objects.min {|a, b| (a.x - player.x).abs <=> (b.x - player.x).abs}
+      if (closest_projectile.x - player.x).abs < self.width
+        accelerate_left if closest_projectile.x > self.x
+        accelerate_right if closest_projectile.x < self.x
+      end
+      # Check if must dodge closest missile
+      # Line -nearly- up with target
+      # Fire at target
     end
 
     def target(target)
