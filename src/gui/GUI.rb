14ae34b62e49963e712ca3bd395f3b42cfd00b98
Freddan962 <fredde.sander@gmail.se>
2014-05-27 20:37:00 +0200

    GUI is now part of the GUI module

--- src/gui/GUI.rb
+++ src/gui/GUI.rb
@@ -1,20 +1,22 @@
-class GUI
-  def initialize(visible = true)
-    @visible = visible
-    @items = []
-  end
+module GUI
+  class GUI
+    def initialize(visible = true)
+      @visible = visible
+      @items = []
+    end
 
-  def draw
-    if @visible and @items.size > 0
-      @items.each do |item|
-        item.draw
+    def draw
+      if @visible and @items.size > 0
+        @items.each do |item|
+          item.draw
+        end
       end
     end
-  end
 
-  def update
-  end
+    def update
+    end
 
-  def add_item(image, x, y)
+    def add_item(image, x, y)
+    end
   end
 end
