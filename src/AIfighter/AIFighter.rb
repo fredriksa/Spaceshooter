509d4ebd3ed5435cdfc197b0e4e42dfbc0c5b731
Fredrik Sander <fredde.sander@gmail.com>
2014-05-28 15:13:13 +0200

    Saves only class type Projectile

--- src/AIfighter/AIFighter.rb
+++ src/AIfighter/AIFighter.rb
@@ -2,7 +2,7 @@ module AIFighter
   class AIFighter < SpaceshipFighter
     def update(objects)
       @objects = objects.dup
-      clean_from_class(@objects, Projectile)
+      @objects = clean_from_class(@objects, Projectile, true)
       if (closest_projectile.x - player.x).abs < self.width
         accelerate_left if closest_projectile.x > self.x
         accelerate_right if closest_projectile.x < self.x
@@ -17,6 +17,7 @@ module AIFighter
     end
 
     def clean_from_class(objects, class_type, opposite = false)
+      objects = objects.dup
       return objects.drop_while {|object| object.class == class_type} if opposite == false
       return objects.drop_while {|object| object.class != class_type} if opposite == true
     end
