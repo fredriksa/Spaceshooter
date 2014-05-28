17505b712f0c4ac7926bb85c5ace438793670d73
Fredrik Sander <fredde.sander@gmail.com>
2014-05-28 14:39:35 +0200

    Returns the closest projectile

--- src/AIfighter/AIFighter.rb
+++ src/AIfighter/AIFighter.rb
@@ -3,7 +3,6 @@ module AIFighter
     def update(objects)
       @objects = objects.dup
       objects.drop_while {|object| object.class != Projectile && object.team != "Player"}
-      closest_projectile = objects.min {|a, b| (a.x - player.x).abs <=> (b.x - player.x).abs}
       if (closest_projectile.x - player.x).abs < self.width
         accelerate_left if closest_projectile.x > self.x
         accelerate_right if closest_projectile.x < self.x
@@ -18,7 +17,7 @@ module AIFighter
     end
 
     def closest_projectile
-
+      @objects.min {|a, b| (a.x - player.x).abs <=> (b.x - player.x).abs}
     end
   end
 end
