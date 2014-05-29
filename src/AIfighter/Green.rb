85d9400762bcc233e88651c5d5ff355bf2df7a57
Fredrik Sander <fredde.sander@gmail.com>
2014-05-30 01:26:26 +0200

    Assigned new default value for speed_damper

--- src/AIfighter/Green.rb
+++ src/AIfighter/Green.rb
@@ -1,6 +1,6 @@
 module AIFighter
   class Green < AIFighter
-    def initialize(images, projectile_image, x, y, speed, type, speed_damper = 1)
+    def initialize(images, projectile_image, x, y, speed, type, speed_damper = 0.95)
       super images, projectile_image, x, y, speed, type, true, speed_damper
     end
   end
