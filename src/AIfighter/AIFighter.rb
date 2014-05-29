44e37dd19c983da221e0a35a2d8b35a6fefebcad
Fredrik Sander <fredde.sander@gmail.com>
2014-05-30 01:26:13 +0200

    AI now has proper movement when dodging

--- src/AIfighter/AIFighter.rb
+++ src/AIfighter/AIFighter.rb
@@ -1,15 +1,21 @@
 module AIFighter
   class AIFighter < SpaceshipFighter
     include RectangularCollision
-    def initialize(images, projectile_image, x, y, speed, type, dodge_projectiles = true, speed_damper = 1)
+    def initialize(images, projectile_image, x, y, speed, type, dodge_projectiles = true, speed_damper = 0.95)
       super images, projectile_image, x, y, speed, type, speed_damper
       @property = {"dodge_projectiles" => dodge_projectiles}
-      @static_timer = {"dodge_projectiles" => 30}
+      @static_timer = {"dodge_projectiles" => 5}
       @timer = @static_timer.dup
     end
 
     def update(objects)
+      super()
       @objects = objects
+      
+      accelerate_left if @accelerate_left == true
+      accelerate_right if @accelerate_right == true
+      move_x if @x + @velocity_x > 0 && @x + @velocity_x + width < GameWindow::WIDTH
+
       dodge_projectile if @property["dodge_projectiles"]
     end
     
@@ -19,10 +25,12 @@ module AIFighter
 
     def dodge_projectile
       if @timer["dodge_projectiles"] > @static_timer["dodge_projectiles"]
+        @accelerate_left = false
+        @accelerate_right = false
+
         projectiles = clean_from_class(@objects, Projectile, true)
         threatening_projectiles = threatening(projectiles)
         closest_projectile = closest_entity(threatening_projectiles)
-        
         if closest_projectile != nil
           dodge(closest_projectile)
           @timer["dodge_projectiles"] = 0
@@ -33,17 +41,13 @@ module AIFighter
     end
 
     def dodge(entity)
-      puts "dodged"
-      accelerate_left if entity.x > self.x
-      accelerate_right if entity.x < self.x
+      @accelerate_left = true if entity.x + entity.width/2 > self.x + self.width/2
+      @accelerate_right = true if entity.x + entity.width/2 < self.x + self.width/2
     end
 
     def might_collide?(entity)
       entity = entity.dup
       entity.y = self.y
-      puts entity.class
-      print "X: #{self.x} Y: #{self.y}, EX: #{entity.x} EY: #{entity.y}"
-      print "WIDTH: #{self.width} HEIGHT: #{self.height}, EWIDTH: #{entity.width} EHEIGHT: #{entity.height}"
       rectangular_collision?(self, entity)
     end
 
