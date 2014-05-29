e2e3e15a02c737277ac818fc90d46366b3267e59
Fredrik Sander <fredde.sander@gmail.com>
2014-05-29 21:53:44 +0200

    Added constructor arguments

--- src/AIfighter/Green.rb
+++ src/AIfighter/Green.rb
@@ -1,6 +1,6 @@
 module AIFighter
   class Green < AIFighter
-    def initialize
+    def initialize(images, projectile_image, x, y, speed, type, dodge_projectiles = true, speed_damper = 1)
 
     end
   end
