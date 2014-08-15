269648d0b0a859f9a2ba9e72081c074940ec4f7e
Fredrik Sander <fredde.sander@gmail.com>
2014-08-15 19:21:18 +0200

    Sat min and max horizontal velocity

--- src/SpaceShipFighter.rb
+++ src/SpaceShipFighter.rb
@@ -3,12 +3,20 @@ class SpaceshipFighter < FloatingEntity
   
   def initialize(images, projectile_image, x, y, speed, type, speed_damper = 1)
     super images, x, y, speed, speed_damper
+    @max_velocity_x, @min_velocity_x = 5, -5
     @projectile_image = projectile_image
     @type = type
     @projectiles = []
     @damage = 25
     @ammo = 0
     @solid = true
+    
+    case type
+    when "Player"
+      @projectile_y = -3
+    when "Hostile"
+      @projectile_y = 3
+    end
   end
 
   def draw
@@ -34,6 +42,6 @@ class SpaceshipFighter < FloatingEntity
   end
 
   def fire
-    @projectiles << Projectile.new(@projectile_image, @x + @width/2 - 2, @y, 1.5, -3, @type)
+    @projectiles << Projectile.new(@projectile_image, @x + @width/2 - 2, @y, 1.5, @projectile_y, @type)
   end
 end
