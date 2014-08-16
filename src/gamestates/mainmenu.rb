4ae04acf44956e947ece65aee638e0ce2672c759
Freddan962 <fredde.sander@gmail.se>
2014-08-16 15:05:27 +0200

    Added the essential GUI elements

--- src/gamestates/mainmenu.rb
+++ src/gamestates/mainmenu.rb
@@ -5,7 +5,9 @@ class MainMenu < GameState
     @GUI.add_text("Title", "Spaceshooter", 50, 50)
 
     @GUI.set_font(@rm["hyperspace_24"])
-    @GUI.add_text("Play", "Play", 50, 100)
+    @GUI.add_text("Play", "Play", 50, 125)
+    @GUI.add_text("Settings", "Settings", 50, 160)
+    @GUI.add_text("Multiplayer", "Multiplayer", 50, 195)
   end
 
   def draw
