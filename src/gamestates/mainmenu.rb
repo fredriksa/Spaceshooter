0e5db7dc5cf6bc069d00407a1b6a19cd8455e499
Freddan962 <fredde.sander@gmail.se>
2014-08-16 15:26:54 +0200

    Added new icon under the title

--- src/gamestates/mainmenu.rb
+++ src/gamestates/mainmenu.rb
@@ -3,6 +3,7 @@ class MainMenu < GameState
     super window
     @GUI.set_font(@rm["hyperspace"])
     @GUI.add_text("Title", "Spaceshooter", 50, 50)
+    @GUI.add_icon("white_rectangle_183px", @rm["white_rectangle_183px"], 50, 85)
 
     @GUI.set_font(@rm["hyperspace_24"])
     @GUI.add_text("Play", "Play", 50, 175)
@@ -25,12 +26,12 @@ class MainMenu < GameState
     if type == "up"
       case key
       when Gosu::MsLeft
-        
+
       end
     end
   end
 
   def gui_collision?
-      
+
   end
 end
