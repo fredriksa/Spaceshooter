d3000a271092e96ce12a2efe6bba8951beaa2c22
Freddan962 <fredde.sander@gmail.se>
2014-08-16 15:26:36 +0200

    Removed inheritance from entity, added update method

--- src/gui/Icon.rb
+++ src/gui/Icon.rb
@@ -1,7 +1,15 @@
 module GUI
-  class Icon < Entity
+  class Icon
+    def initialize(image, x, y)
+      @image = image
+      @x, @y = x, y
+    end
+
     def draw
       @image.draw(@x, @y, ZOrder::GUI_Icon)
+    end
+
+    def update 
     end
   end
 end
