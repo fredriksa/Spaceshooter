cd43ddbb6470e4a10eeec7bf939353d3357756a2
Freddan962 <fredde.sander@gmail.se>
2014-08-16 15:03:05 +0200

    Added basic GUI elements

--- src/gamestates/mainmenu.rb
+++ src/gamestates/mainmenu.rb
@@ -1,4 +1,13 @@
 class MainMenu < GameState
+  def initialize(window)
+    super window
+    @GUI.set_font(@rm["hyperspace"])
+    @GUI.add_text("Title", "Spaceshooter", 50, 50)
+
+    @GUI.set_font(@rm["hyperspace_24"])
+    @GUI.add_text("Play", "Play", 50, 100)
+  end
+
   def draw
     super
   end
