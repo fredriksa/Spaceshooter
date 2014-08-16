c527c69f5dd47ddd080841b6551fcab4c715ab79
Freddan962 <fredde.sander@gmail.se>
2014-08-16 16:04:47 +0200

    New method collision_mouse?

--- src/RectangularCollisionModule.rb
+++ src/RectangularCollisionModule.rb
@@ -13,4 +13,13 @@ module RectangularCollision
     ((object1_left >= object2_left && object1_left <= object2_right) || (object2_left >= object1_left && object2_left <= object1_right)) &&
     ((object1_top >= object2_top && object1_top <= object2_bottom) || (object2_top >= object1_top && object2_top <= object1_bottom))
   end
+
+  def collision_mouse?(object, x, y)
+    object_left = object.x
+    object_right = object.x + object.width
+    object_top = object.y
+    object_bottom = object.y + object.height
+
+    (object_left < x && object_right > x && object_top < y && object_bottom > y)
+  end
 end
